961af45b21482bf4a7aaf6b4a62644780492c9e6
matthewgreer <matthew.greer.dev@gmail.com>
2021-03-11 08:48:52 -0500

    completed phases 1, 2 of w8d3 js

---
+++ javascript/w8d3/intro_js_problems/phase_1_arrays.js
@@ -0,0 +1,109 @@
+Array.prototype.uniq = function() {
+  const uniqArray = [];
+  this.forEach((ele) => {
+    if (!uniqArray.includes(ele)) uniqArray.push(ele);
+  })
+  return uniqArray;
+};
+
+// const arr1 = [1, 1, 2, 2, 2, 3, 4, 5, 5, 5];
+// const arr2 = [];
+// const arr3 = [9, "butt", "butt", "arse", 39];
+
+// console.log(arr1.uniq());
+// console.log(arr2.uniq());
+// console.log(arr3.uniq());
+
+Array.prototype.twoSum = function() {
+  const sumToZero = [];
+  for (let i = 0; i < this.length - 1; i ++) {
+    for (let j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) sumToZero.push([this[i], this[j]]);
+    }
+  }
+  return sumToZero;
+};
+
+// const arr4 = [];
+// const arr5 = [1, 2, 3, 4, -1, -3, 0];
+// const arr6 = [1, 0, 4, -1, -3, 2, -2, 8];
+
+// console.log(arr4.twoSum())
+// console.log(arr5.twoSum())
+// console.log(arr6.twoSum())
+
+
+Array.prototype.transpose = function() {
+  return this[0].map((_,colIdx) => this.map(row => row[colIdx]));
+};
+
+// const arr7 = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]];
+// const arr8 = [
+//   ["A", "B", "C", "D"],
+//   ["a", "b", "c", "d"]
+// ];
+
+// console.log(arr7.transpose());
+// console.log(arr7);
+// console.log(arr8.transpose());
+// console.log(arr8);
+
+
+// WALKTHROUGH OF arr8.transpose();
+// ================================
+// this[0] is arr8 at index 0, which is ["A", "B", "C", "D"]
+// iterate & evaluate:
+//  [
+//   first element "A" (index 0) =>
+//          this is arr8, which is [["A", "B", "C", "D"],["a", "b", "c", "d"]
+//          iterate & evaluate:
+//            [
+//             ["A", "B", "C", "D"] => ["A", "B", "C", "D"] at index 0, so: "A"
+//             ["a", "b", "c", "d"] => ["a", "b", "c", "d"] at index 0, so: "a"
+//            ]
+//   so "A" => ["A", "a"]
+//   ,
+//   second element "B" (index 1) =>
+//          this is arr8, which is [["A", "B", "C", "D"],["a", "b", "c", "d"]
+//          iterate & evaluate:
+//            [
+//             ["A", "B", "C", "D"] => ["A", "B", "C", "D"] at index 1, so: "B"
+//             ["a", "b", "c", "d"] => ["a", "b", "c", "d"] at index 1, so: "b"
+//            ]
+//   so "B" => ["B", "b"]
+//   ,
+//   third element "C" (index 2) =>
+//          this is arr8, which is [["A", "B", "C", "D"],["a", "b", "c", "d"]
+//          iterate & evaluate:
+//            [
+//             ["A", "B", "C", "D"] => ["A", "B", "C", "D"] at index 2, so: "C"
+//             ["a", "b", "c", "d"] => ["a", "b", "c", "d"] at index 2, so: "c"
+//            ]
+//   so "C" => ["C", "c"]
+//   ,
+//   fourth element "D" (index 3) =>
+//          this is arr8, which is [["A", "B", "C", "D"],["a", "b", "c", "d"]
+//          iterate & evaluate:
+//            [
+//             ["A", "B", "C", "D"] => ["A", "B", "C", "D"] at index 3, so: "D"
+//             ["a", "b", "c", "d"] => ["a", "b", "c", "d"] at index 3, so: "d"
+//            ]
+//   so "D" => ["D", "d"]
+//   ]
+//   so arr8.transpose(); => [["A","a"],["B","b"],["C","c"],["D","d"]]
+
+//  arr8
+  //  [
+  //    ["A", "B", "C", "D"],
+  //    ["a", "b", "c", "d"]
+  //  ];
+
+// arr8.transpose();
+  // [
+  //   ["A", "a"],
+  //   ["B", "b"],
+  //   ["C", "c"],
+  //   ["D", "d"],
+  // ];
+
+
---
+++ javascript/w8d3/intro_js_problems/phase_2_enumerable.js
@@ -0,0 +1,60 @@
+Array.prototype.myEach = function(callback) {
+
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+
+  return undefined;
+
+};
+
+const arr1 = [1, 5, 10, 20, 100];
+const arr2 = ['cat', 'bird', 'hippo'];
+const cb1 = function(num) {
+  return num + 5;
+};
+const cb2 = function(str) {
+  return (`the ${str} ate my nuts`);
+};
+const cb3 = function(acc, num) {
+  return acc * num;
+};
+const cb4 = function(acc, num) {
+  return acc + num;
+};
+
+console.log(arr1.myEach(cb1));
+console.log(arr2.myEach(cb2));
+
+Array.prototype.myMap = function(callback) {
+  
+  let newArray = [];
+
+  this.myEach((el) => {
+    newArray.push(callback(el));
+  })
+
+  return newArray;
+
+};
+
+console.log(arr1.myMap(cb1));
+console.log(arr2.myMap(cb2));
+
+Array.prototype.myReduce = function(callback, initialValue) {
+  
+  let reducerArray = this.slice();
+  let accumulator = initialValue ? initialValue : reducerArray.shift();
+  
+  reducerArray.myEach((el) => {
+    accumulator = callback(accumulator, el); 
+  })
+
+  return accumulator;
+
+}
+
+console.log(arr1.myReduce(cb4));
+console.log(arr1.myReduce(cb4, 12));
+console.log(arr1.myReduce(cb3));
+console.log(arr1.myReduce(cb3, 4));
