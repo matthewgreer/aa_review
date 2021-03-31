3bab7a4fc1704e6d9ee4bac8a6dddf19dcfd6250
matthewgreer <matthew.greer.dev@gmail.com>
2021-03-31 17:07:53 -0400

    refactor w1d1 coprime

--- foundations/w1d1/advanced_methods_exercise/01_coprime.rb
+++ foundations/w1d1/advanced_methods_exercise/01_coprime.rb
@@ -3,20 +3,11 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
-def coprime?(num_1, num_2)
-  fct2 = factors(num_2)
-  factors(num_1).none? do |f1|
-    fct2.include?(f1)
-  end
+def coprime?(num_1, num_2) 
+  # choose to iterate through the smaller range to save time
+  num_1, num_2 = num_2, num_1 if num_1 > num_2
+  (2..num_1).none? { |divisor| num_1 % divisor == 0 && num_2 % divisor == 0 }
 end
-
-def factors(num)
-  fct = []
-  (2..num).each { |el| fct << el if num % el == 0 }
-  fct
-end
-
-
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
