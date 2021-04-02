cfa09ce84fff75f1d4c67f6b2b66a39b1b71abe0
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-02 16:36:20 -0400

    Make valid_colors instance variable

--- rails/w6d4/ninety-nine_cats/app/models/cat.rb
+++ rails/w6d4/ninety-nine_cats/app/models/cat.rb
@@ -19,15 +19,15 @@ class Cat < ApplicationRecord
   extend ActionView::Helpers::DateHelper
   
   validates :birth_date, presence: true
-  validates :color, presence: true, inclusion: { in: VALID_COLORS }, 
-    message: {"Sorry. Our feline overlords' only valid colors are #{VALID_COLORS}."}
+  validates :color, presence: true, inclusion: { in: @valid_colors }, 
+    message: {"Sorry. Our feline overlords' only valid colors are #{@valid_colors}."}
   validates :name, presence: true, uniqueness: true
     message: {"Whoa, %{value} is already in our system. Please choose a unique name for your ${value}"}
   validates :sex, presence: true, length: { is: 1 }, inclusion: { in: ["M", "F"]}, 
     message: {"Sorry. Our feline overlords demand a choice between 'M' and 'F'."}
   validates :description, presence: true
 
-  VALID_COLORS = ['i', 'remember', 'nothing', 'from', 'this', 'course', 'fml']
+  @valid_colors = ['black', 'brown', 'calico', 'gray/blue', 'tabby', 'tortoiseshell', 'tuxedo', 'white']
 
   def age
     time_ago_in_words(:birth_date)
