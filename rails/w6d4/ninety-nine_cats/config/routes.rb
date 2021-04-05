e9057abeaa82f03277ffaf34a27ca0eb67c2fd49
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-05 14:56:56 -0400

    Fix root routing

--- rails/w6d4/ninety-nine_cats/config/routes.rb
+++ rails/w6d4/ninety-nine_cats/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats
 
-  root to: redirect_to(cats_url)
+  root to: "cats#index"
 end
