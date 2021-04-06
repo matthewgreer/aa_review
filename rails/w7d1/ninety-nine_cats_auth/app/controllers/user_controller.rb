493a54d6e66d2b8dc7b39771dc33708e0777d006
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-06 16:49:09 -0400

    Create Users controller

---
+++ rails/w7d1/ninety-nine_cats_auth/app/controllers/user_controller.rb
@@ -0,0 +1,25 @@
+class UserController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :root
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+
+end
