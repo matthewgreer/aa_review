82a4c27b427597e05159236632f04d8203920898
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-06 17:45:17 -0400

    Create sessions controller

--- rails/w7d1/ninety-nine_cats_auth/app/controllers/sessions_controller.rb
+++ rails/w7d1/ninety-nine_cats_auth/app/controllers/sessions_controller.rb
@@ -1,13 +1,28 @@
 class SessionsController < ApplicationController
 
+
+
   def new
     @session = Session.new
     render :new
   end
 
   def create
-    @user = 
-    
+    @user = User.find_by_credentials(
+      params[:user][user_name],
+      params[:user][user_name]
+    )
+    if @user.nil?
+     flash.now[:errors] = @user.errors.full_messages
+     render :new
+    else
+      login!(@user)
+      redirect_to cats_url
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
   end
 
 end
