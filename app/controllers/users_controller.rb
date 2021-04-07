8ad77c951255610c3ed9eb22feecbae057480df4
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-07 18:23:55 -0400

    Create Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      login!(@user)
+      redirect_to :root
+    else
+      flash.now(@user.errors.full_messages)
+      render :new
+    end
+  end
+
+  private
+
+  def user_params(params)
+    params.require(:user).permit(:user_name, :password)
+  end
+
+end
