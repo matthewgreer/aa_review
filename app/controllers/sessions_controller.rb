672abb0add3a1c555134140effaccc03bb9b1980
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-08 12:24:21 -0400

    Create sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
+    render :new
   end
 
   def create
+    user = User.find_by(params[:user][:email], params[:user][:password])
+    if user
+      login!(user)
+      redirect_to user_url
+    else
+      render :new
+    end
   end
 
   def destroy
+    self.logout!
+    redirect_to new_session_url
   end
 end
