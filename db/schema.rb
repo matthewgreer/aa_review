eef612d16d9753ae5704ab15caa8eb91dda05dca
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-09 11:10:57 -0400

    Add null constraints to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_215205) do
+ActiveRecord::Schema.define(version: 2021_04_08_225021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "session_token"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
