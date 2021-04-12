097825a824f967e72e1ed0f5c0361168e6b1928f
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-11 20:45:58 -0400

    Define Order controller and model methods

--- rails/widget_challenge_app/app/controllers/orders_controller.rb
+++ rails/widget_challenge_app/app/controllers/orders_controller.rb
@@ -1,12 +1,34 @@
 class OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
     render :show
   end
 
   def new
+    @order = Order.new
     render :new
   end
 
   def create
+    if order_params[:order[:date_needed]].is_valid_date?
+      @order = Order.new(order_params)
+      if @order.save
+        redirect_to order_url(@order)
+      else
+        flash.now[:errors] = @order.errors.full_messages
+        render :new
+      end
+    else
+      flash.now[:errors] = ['Date must be 7 or more days from today.']
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :quantity, :date_needed, :color, :order_type
+    )
+  end
+
 end
--- rails/widget_challenge_app/app/models/order.rb
+++ rails/widget_challenge_app/app/models/order.rb
@@ -1,8 +1,19 @@
 class Order < ApplicationRecord
 
-  validates :identifier, :quantity, :date_needed, :color, :type, presence: true
-  validates :identifier, uniqueness: true
+  validates :quantity, :date_needed, :color, :type, presence: true
+  validates :identifier, presence: true, uniqueness: true
   validates :color, inclusion: {in: ['red', 'yellow', 'blue']}
-  validates :type, inclusion: {in: ['Widget', 'Widget Pro', 'Widget Xtreme']}
+  validates :order_type, inclusion: {in: ['Widget', 'Widget Pro', 'Widget Xtreme']}
+  
+  after_initialize :set_unique_identifier
 
+  def set_unique_identifier
+    self.identifier = SecureRandom::urlsafe_base64(10)
+  end
+  
+  def is_valid_date?(date)
+    today = Date.current
+    (date - today).to_i >= 7
+  end
+  
 end
