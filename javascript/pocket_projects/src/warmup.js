a10fddc1f918ef43c2653f4c92bd83313636c40d
matthewgreer <matthew.greer.dev@gmail.com>
2021-04-15 14:12:55 -0400

    Complete phase 0 warmup

--- javascript/pocket_projects/src/warmup.js
+++ javascript/pocket_projects/src/warmup.js
@@ -2,7 +2,9 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+  const newParagraph = document.createElement('p');
+  newParagraph.innerText = string;
+  return htmlElement.appendChild(newParagraph);
 };
 
 htmlGenerator('Party Time.', partyHeader);
